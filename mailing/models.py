b9efc2f4091cfd755f172532d802a43a34150b62
ildar-vk <kalimov.ildar.v@gmail.com>
2026-03-18 05:57:22 +0300

    Создал модель клиента применил миграции

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Client(models.Model):
+    email = models.EmailField(unique = True, verbose_name = "Электронная почта")
+    full_name =models.CharField(max_length = 150, verbose_name = "ФИО")
+    comment = models.TextField(blank =True, null = True, verbose_name = "Комментарии")
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete =models.CASCADE,
+                             blank = True, null = True, verbose_name = "Владелец")
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+    def __str__(self):
+        return f"{self.full_name} ({self.email})"
+
+
+
+
+
+
+
