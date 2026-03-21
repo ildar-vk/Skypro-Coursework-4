aaef00acba1eb872b1b79e187181b5d208d6db89
ildar-vk <kalimov.ildar.v@gmail.com>
2026-03-22 00:36:46 +0300

    Написал модель сообщений и применил миграции

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email','full_name','owner')
+    search_fields = ('email','full_name')
--- mailing/models.py
+++ mailing/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
 
+
+
 class Client(models.Model):
     email = models.EmailField(unique = True, verbose_name = "Электронная почта")
     full_name =models.CharField(max_length = 150, verbose_name = "ФИО")
-    comment = models.TextField(blank =True, null = True, verbose_name = "Комментарии")
+    comment = models.TextField(blank = True, null = True, verbose_name = "Комментарии")
 
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete =models.CASCADE,
                              blank = True, null = True, verbose_name = "Владелец")
@@ -15,9 +17,18 @@ class Client(models.Model):
     def __str__(self):
         return f"{self.full_name} ({self.email})"
 
+class Message(models.Model):
+    theme = models.CharField(unique = True,max_length = 100, verbose_name = "Тема письма")
+    body = models.TextField(blank = True, null = True, verbose_name = "Тело письма")
 
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE,
+                              blank = True, null = True, verbose_name = "Владелец" )
 
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
 
-
+    def __str__(self):
+        return f"{self.theme} ({self.owner})"
 
 
